063f2258f407fadb45269e86718e318b91e82536
colaboratory <colab@colab.com>
2021-06-30 20:29:15 +0000

    commit

--- utils.py
+++
@@ -1,7 +0,0 @@
-import importlib.resources
-import yaml
-
-def read_yaml(file_name):
-  with importlib.resources.path("recochef.config", file_name) as file_path:
-    with open(file_path, "r") as f:
-      return yaml.safe_load(f)
