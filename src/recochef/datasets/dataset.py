ce0e7306c7ec1fa429412263db2a5a1bcb1101b1
colaboratory <colab@colab.com>
2021-06-30 16:01:22 +0000

    commit

--- src/recochef/datasets/dataset.py
+++ src/recochef/datasets/dataset.py
@@ -1,11 +1,14 @@
 from data_cache import pandas_cache
 import warnings
+import os
 
 from recochef.utils._utils import read_yaml
 
 warnings.filterwarnings('ignore')
 
-parmalink_path = '../config/parmalink.yaml'
+current_dir = os.path.dirname(os.path.dirname(__file__))
+parent_dir = os.path.dirname(current_dir)
+parmalink_path = os.path.join(parent_dir, 'config', 'parmalink.yaml')
 permalinks = read_yaml(parmalink_path)
 
 class Dataset:
