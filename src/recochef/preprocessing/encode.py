04d2c70e6d76cbe332368a104999c0f5f0ae6b45
colaboratory <colab@colab.com>
2021-07-03 08:44:13 +0000

    commit

---
+++ src/recochef/preprocessing/encode.py
@@ -0,0 +1,10 @@
+def label_encode(data, cols):
+  maps = {}
+  for col in cols:
+    _unique = data[col].unique()
+    _map = {old: new for new, old in enumerate(_unique)}
+    _reverse_map = {new: old for new, old in enumerate(_unique)}
+    data[col] = data[col].map(_map)
+    maps[col+'_TO_IDX'] = _map
+    maps['IDX_TO_'+col] = _reverse_map
+  return data, maps
